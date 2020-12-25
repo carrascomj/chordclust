d67378db6af15daf6938334b270bd64c387ba291
carrascomj <carrascomurielj@gmail.com>
2020-12-25 20:00:47 +0100

    docs: add example :memo:

---
+++ examples/from_file.rs
@@ -0,0 +1,12 @@
+use bclust::cluster_similarity;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() {
+    let f = File::open("examples/UP000000425_122586_DNA_sample.fasta").unwrap();
+    let reader = BufReader::new(f);
+    let cluster_db = cluster_similarity(reader, 8, 85);
+    println!("{:#?}", cluster_db)
+
+}
+
