40e2658dcc796b6ae4fd8ae9b1090c7689762bc6
carrascomj <carrascomurielj@gmail.com>
2020-12-28 02:37:01 +0100

    docs: improve hierarchical docs :bulb:

--- src/hierarchical.rs
+++ src/hierarchical.rs
@@ -4,20 +4,14 @@ use super::cluster::BucketCluster;
 /// threshold around the centroids. The centroids are then passed to a second
 /// clustering process as sequences to be clustered with a lower treshold.
 ///
-/// # Examples
-/// ```
-/// use std::fs::File;
-/// use std::io::BufReader;
-/// use chordclust::{read_fasta_sorted, cluster_slice};
+/// Given that all the members of a cluster are centroids of the clusters in
+/// the previous iteration, the final structure is built by expanding the lowest
+/// similiarity cluster with its parents until the higher similarity cluster
+/// is processed.
 ///
-/// let f = File::open("examples/UP000000425_122586_DNA_sample.fasta").unwrap();
-/// let reader = BufReader::new(f);
-/// let sequences = read_fasta_sorted(reader);
-/// let cluster_db = cluster_slice(&sequences, 8, 85);
-/// let n_clusters = cluster_db.clusters.len();
-/// assert!(1 < n_clusters);
-/// assert!(n_clusters < sequences.len());
-/// ```
+/// See an [example on the repository](https://github.com/carrascomj/chordclust/blob/trunk/examples/hierarchical.rs).
+///
+/// Internally, this is implemented starting from the top with dynamic programming.
 pub fn cluster_hierarchical<'a>(
     sequences: &'a [String],
     k: usize,
@@ -27,6 +21,7 @@ pub fn cluster_hierarchical<'a>(
     for seq in sequences.iter() {
         cluster_db.push(seq);
     }
+    // perform all hierarchical steps from the previous centroids
     let mut cluster_dbs: Vec<BucketCluster> = Vec::with_capacity(similarity_thresholds.len() - 1);
     cluster_dbs.push(cluster_db);
     for (i, &threshold) in similarity_thresholds.iter().enumerate() {
@@ -38,7 +33,10 @@ pub fn cluster_hierarchical<'a>(
             .for_each(|seq| cluster_db.push(seq));
         cluster_dbs.push(cluster_db);
     }
+    // start from the second highest cluster, expanding the clusters 
+    // from their parents (which are centroids of the previous cluster)
     cluster_dbs.reverse();
+    // we keep the cluster above the one being expanded
     let mut clust_above = cluster_dbs.pop().unwrap();
     while let Some(mut bucket) = cluster_dbs.pop() {
         bucket.clusters.iter_mut().for_each(|(_, cluster)| {
